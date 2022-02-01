e84be07e2dd776781201ec4a8ef08f6aebbd523f
arq99 <aqureshi1028@gmail.com>
2022-02-01 17:21:35 -0500

    updated to run with docker

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
 FROM python:3.7.8-slim
 
-COPY requirements/common.txt requirements/common.txt
-RUN pip install -U pip && pip install -r requirements/common.txt
+COPY requirements.txt requirements.txt
+RUN pip install -U pip && pip install -r requirements.txt
 
 COPY ./api /app/api
 COPY ./bin /app/bin
+COPY ./database /app/database
 COPY wsgi.py /app/wsgi.py
 WORKDIR /app
 
--- api/app.py
+++ api/app.py
@@ -1,21 +1,4 @@
-import os
-import json
-import string
-
-from flask import Flask, Response, request, abort
-
-from dotenv import load_dotenv, find_dotenv
-
-from newsapi import NewsApiClient
-
-from nltk.tokenize import TreebankWordTokenizer
-from nltk.stem.porter import PorterStemmer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-
-import pandas as pd
-
-import numpy as np
+from flask import Flask, Response, request
 
 from database.mongodb import MongoDB
 
@@ -23,11 +6,6 @@ from .errors import errors
 
 app = Flask(__name__)
 app.register_blueprint(errors)
-
-load_dotenv(find_dotenv())
-NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
-
-newsapi = NewsApiClient(api_key=NEWS_API_KEY)
 
 connection = MongoDB.get_connection()
 db = connection.newsarticles
